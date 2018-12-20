10b7f9d6d504709adffc39a73c2a379dbc07563c
airdur <airdur@AirBook>
2018-12-20 16:13:26 +0100

    fix 4ac96bf

--- Combat-pour-la-vie/src/consommateur/Caribou.java
+++ Combat-pour-la-vie/src/consommateur/Caribou.java
@@ -2,4 +2,9 @@ package consommateur;
 
 public class Caribou extends Cervide{
 
+	public Caribou(Sexe s, int v) {
+		super(s, v);
+		// TODO Auto-generated constructor stub
+	}
+
 }
--- Combat-pour-la-vie/src/consommateur/Chevreuil.java
+++ Combat-pour-la-vie/src/consommateur/Chevreuil.java
@@ -2,4 +2,9 @@ package consommateur;
 
 public class Chevreuil extends Cervide{
 
+	public Chevreuil(Sexe s, int v) {
+		super(s, v);
+		// TODO Auto-generated constructor stub
+	}
+
 }
