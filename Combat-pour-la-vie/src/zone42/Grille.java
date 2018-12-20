555044a1ab22b562bbc927e565bef27a9abe85fa
airdur <airdur@AirBook>
2018-12-20 16:28:51 +0100

    add grille.java

--- Combat-pour-la-vie/src/zone42/Grille.java
+++ Combat-pour-la-vie/src/zone42/Grille.java
@@ -2,4 +2,10 @@ package zone42;
 
 public class Grille {
 
+	private Integer x;
+	private Integer y;
+	
+	public Grille() {
+		
+	}
 }
--- Combat-pour-la-vie/src/zone42/Zone42.java
+++ Combat-pour-la-vie/src/zone42/Zone42.java
@@ -6,8 +6,26 @@ import consommateur.Consommateur;
 public class Zone42 {
 	private boolean lock;
 	private ArrayList<Consommateur> list_consommateur;
+	private Grille grille;
+	
+	public Zone42() {
+		grille = new Grille();
+	}
 	
 	public void faire_passer_le_temps() {};
-	public void lock() {}
-	public void delock() {}
+	
+	public void lock() {
+		setLock(true);
+	}
+	public void delock() {
+		setLock(false);
+	}
+
+	public boolean isLock() {
+		return lock;
+	}
+
+	public void setLock(boolean lock) {
+		this.lock = lock;
+	}
 }
