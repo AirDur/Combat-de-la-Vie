97c681e3f6146377a7cba3109c458a0de976b611
airdur <airdur@AirBook>
2019-01-03 15:58:00 +0100

    commentaires

--- Combat-pour-la-vie/src/zone42/Fabrique_de_Vegetaux.java
+++ Combat-pour-la-vie/src/zone42/Fabrique_de_Vegetaux.java
@@ -2,6 +2,13 @@ package zone42;
 
 import aliment.*;
 
+/**
+ * Implémentation du design pattern de Fabrique. Cette classe est une fabrique qui instancie des vegetaux du type passé 
+ * en paramètre du constructeur.
+ * 
+ * @author Romain Duret
+ *
+ */
 public abstract class Fabrique_de_Vegetaux {
 
 	/**
@@ -13,23 +20,36 @@ public abstract class Fabrique_de_Vegetaux {
 	 */
 	private EtatFabrique etat;
 	
-	public Fabrique_de_Vegetaux(int i) {
+	/**
+	 * Type de vegetaux que produit la fabrique
+	 */
+	private TypeVegetaux type;
+	
+	public Fabrique_de_Vegetaux(int i, TypeVegetaux tv) {
 		duree_de_vie = i;
+		type = tv;
 		if(i > 0) 
 			etat = EtatFabrique.enmarche;
 		else
 			etat = EtatFabrique.enpanne;
 	}
 	
-	public Vegetaux creerVegetaux(TypeVegetaux type) {
-		Vegetaux vegetal = null;
-        switch(type)
-        {
-            case foin: vegetal = new Foin(); break;
-            case herbe: vegetal = new Herbe(); break;
-            case plante: vegetal = new Plante(); break;
-            case pomme: vegetal = new Pomme(); break;
-        }
-        return vegetal;
+	/**
+	 * Créer un vegetal. Si la fabrique est en panne, elle retourne null.
+	 * @return vegetal ou null
+	 */
+	public Vegetaux creerVegetaux() {
+		if(etat == EtatFabrique.enmarche) {
+			Vegetaux vegetal = null;
+	        switch(this.type)
+	        {
+	            case foin: vegetal = new Foin(); break;
+	            case herbe: vegetal = new Herbe(); break;
+	            case plante: vegetal = new Plante(); break;
+	            case pomme: vegetal = new Pomme(); break;
+	        }
+	        return vegetal;
+		}
+		else return null;
 	}
 }
