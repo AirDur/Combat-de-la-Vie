0cc7dd443144afea3ed73e27fc11e24ca907961f
airdur <airdur@AirBook>
2019-01-03 15:46:08 +0100

    modification foin pour adaptation

--- Combat-pour-la-vie/src/aliment/Foin.java
+++ Combat-pour-la-vie/src/aliment/Foin.java
@@ -4,8 +4,8 @@ public class Foin extends Vegetaux {
 
 	public static final int propriete_nutritive = 5;
 	
-	public Foin(int valeur, int valeur2) {
-		super(valeur, valeur2);
+	public Foin(int valeur) {
+		super(propriete_nutritive, valeur);
 	}
 
 }
