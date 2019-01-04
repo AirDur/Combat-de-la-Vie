9a1d103e52b4c15d394715df0211001256e50a07
airdur <airdur@AirBook>
2019-01-04 14:54:51 +0100

    commentaire aliment

--- Combat-pour-la-vie/src/aliment/Aliment.java
+++ Combat-pour-la-vie/src/aliment/Aliment.java
@@ -3,17 +3,20 @@ package aliment;
 import zone42.Case;
 
 /**
+ * Ressource à consommer pour les consommateurs. 
  * 
  * @author Romain Duret
- *
  */
 public class Aliment {
 	
 	/**
-	 * 
+	 * Capacité de satisfaire la faim des consommateurs
 	 */
 	private Integer propriete_nutritive;
 	
+	/**
+	 * temps en cycle avant leurs disparitions
+	 */
 	private Integer temps_avant_decomposition;
 	
 	/**
@@ -21,15 +24,25 @@ public class Aliment {
 	 */
 	private Case emplacement;
 	
+	/**
+	 * constructeur vide
+	 */
 	public Aliment() {
 		setPropriete_nutritive(0);
 		setTemps_avant_decomposition(0);
 	}
 	
-	public Aliment(int valeur, int valeur2) 
+	/**
+	 * Constructeur simple
+	 * @param valeur_nutritive Capacité de satisfaire la faim des consommateurs
+	 * @param valeur_conservation temps en cycle avant disparition
+	 * @param c emplacement de l'aliment
+	 */
+	public Aliment(int valeur_nutritive, int valeur_conservation, Case c) 
 	{
-		setPropriete_nutritive(valeur);
-		setTemps_avant_decomposition(valeur);
+		setPropriete_nutritive(valeur_nutritive);
+		setTemps_avant_decomposition(valeur_conservation);
+		setEmplacement(c);
 	}
 
 	public Integer getPropriete_nutritive() {
@@ -46,6 +59,14 @@ public class Aliment {
 
 	public void setTemps_avant_decomposition(Integer temps_avant_decomposition) {
 		this.temps_avant_decomposition = temps_avant_decomposition;
+	}
+
+	public Case getEmplacement() {
+		return emplacement;
+	}
+
+	public void setEmplacement(Case emplacement) {
+		this.emplacement = emplacement;
 	}
 
 }
