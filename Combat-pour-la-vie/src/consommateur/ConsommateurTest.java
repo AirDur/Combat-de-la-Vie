e120a60d00dd46342f19c96e1f71808c161587b0
airdur <airdur@AirBook>
2019-01-17 16:21:51 +0100

    ajout d'un test de reproduction

--- Combat-pour-la-vie/src/consommateur/ConsommateurTest.java
+++ Combat-pour-la-vie/src/consommateur/ConsommateurTest.java
@@ -5,10 +5,14 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class ConsommateurTest {
-
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_reproduction() {
+		Consommateur c = new Loup(Sexe.femelle, 20);
+		
+		Consommateur e = new Loup(Sexe.male, 20);
+		
+		Consommateur f = c.se_reproduire(e);
 	}
 
 }
